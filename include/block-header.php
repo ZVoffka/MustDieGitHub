17d3e5c804503aeb21e10646f6488e1f6b5fdc6f
ZVoffka <Zvoffka777>
2014-07-27 13:24:44 +0300

    В файле block-hexer.php создал «hexer-top-menu»

--- include/block-header.php
+++ include/block-header.php
@@ -1,5 +1,11 @@
 <div id="block-header">
 <div id="header-top-block"></div>
 
+<ul id="heder-top-menu">
+<li>Ваш город - Москва</li>
+<li><a href="o-nas.php">О нас</a></li>
+<li><a href="magaziny.php">Магазины</a></li>
+<li><a href="contacts.php">Котакты</a></li>
+</li>
 
 </div>
