1b9ea4402ff0a9814d8c53de79d1b03528ee5b68
ZVoffka <Zvoffka777>
2014-07-27 16:20:14 +0300

    Добавил в style.css блок #header-top-menu, не получил результат как в уроке.

--- include/block-header.php
+++ include/block-header.php
@@ -2,10 +2,13 @@
 <div id="header-top-block"></div>
 
 <ul id="heder-top-menu">
+
 <li>Ваш город - Москва</li>
 <li><a href="o-nas.php">О нас</a></li>
 <li><a href="magaziny.php">Магазины</a></li>
 <li><a href="contacts.php">Котакты</a></li>
-</li>
+</ul>
 
 </div>
+
+
